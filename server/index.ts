799c84c6d90154e890bdc24697e09d8f554aef3f
Joel <joelsetterberg@hotmail.com>
2022-09-20 20:24:32 +0200

    install: npm init vite-plugin-ssr@latest , with react-ts

--- server/index.ts
+++ server/index.ts
@@ -37,7 +37,7 @@ async function startServer() {
     res.status(statusCode).type(contentType).send(body)
   })
 
-  const port = process.env.PORT || 3000
+  const port = 3013 //process.env.PORT || 3000
   app.listen(port)
   console.log(`Server running at http://localhost:${port}`)
 }
